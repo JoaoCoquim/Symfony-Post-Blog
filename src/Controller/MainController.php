e7c906788873fe0ef9f0a59806b6cd00fc6f6413
jcoquim <joaocoquim@gmail.com>
2023-07-16 17:53:30 +0100

    added bootstrap and twig pages

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -14,22 +14,26 @@ class MainController extends AbstractController
     #[Route('/', name: 'home')]
     public function main(){
 
-        return new Response('<h1>Welcome to Symfony 6.3.1 :)</h1>');
+        return $this->render('home/index.html.twig');
     }
 
-    #[Route('/main/{slug?}', name: 'app_main_slug')]
-    public function indexSlug(Request $request): JsonResponse
+    #[Route('/custom/{slug?}', name: 'custom')]
+    public function indexSlug(Request $request)
     {
 
         $slug = $request->get('slug');
         $variable = "whatever I want here";
 
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MainController.php',
-            'yeah' => $variable,
+        return $this->render('home/custom.html.twig', [
             'slug' => $slug
         ]);
+
+//        return $this->json([
+//            'message' => 'Welcome to your new controller!',
+//            'path' => 'src/Controller/MainController.php',
+//            'yeah' => $variable,
+//            'slug' => $slug
+//        ]);
     }
 
 }
