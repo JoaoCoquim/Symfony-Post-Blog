3dd70feda6bff384c07c9e276f5eff0e2a78a28b
jcoquim <joaocoquim@gmail.com>
2023-07-17 12:23:59 +0100

    added url pages to create and fetch posts from the DB

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,17 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/post', name: 'post.')]
 class PostController extends AbstractController
 {
-    #[Route('/post', name: 'app_post')]
-    public function index(): Response
+    #[Route('/', name: 'index')]
+    public function index(EntityManagerInterface $em): Response
     {
+
+        $repository = $em->getRepository(Post::class);
+        $posts = $repository->findAll();
+
         return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+            'posts' => $posts
         ]);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     */
+    public function create(Request $request, EntityManagerInterface $em){
+        // create a new Post
+        $post = new Post();
+
+        $post->setTitle("My 4th title");
+
+        //entity manager
+        $em->persist($post);
+        $em->flush();
+
+        return $this->render('post/create.html.twig', [
+            'post' => $post
+        ]);
+
     }
 }
